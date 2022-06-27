860006feff50e9fa159ba30b2e726344ece77a0d
Hendrik <mail@hendrikwallbaum.de>
2022-06-27 11:51:37 +0200

    ✨ All Posts render. New fonts.

--- components/GlobalStypes.tsx
+++ components/GlobalStypes.tsx
@@ -16,6 +16,11 @@ export const GlobalStyles = () => (
       //https://fonts.google.com/share?selection.family=Noto%20Emoji%7CNoto%20Sans:ital,wght@0,300;1,300%7CNoto%20Serif
       @import url('https://fonts.googleapis.com/css2?family=Noto+Emoji&family=Noto+Sans:ital,wght@0,300;1,300&family=Noto+Serif&display=swap');
 
+      @font-face {
+        font-family: 'Simonetta-Black';
+        src: url('/fonts/Simonetta-Black.ttf');
+      }
+
       html,
       body {
         padding: 0;
@@ -30,28 +35,28 @@ export const GlobalStyles = () => (
 
       h1 {
         ${scaledFontSize(5)};
-        font-weight: 300;
+        font-family: 'Simonetta-Black', serif;
         margin-bottom: 0;
       }
       h2 {
         ${scaledFontSize(4)};
-        font-weight: 300;
-        font-family: 'Noto Serif', serif;
+        font-weight: 900;
+        font-family: 'Simonetta-Black', serif;
         margin-bottom: 0;
       }
       h3 {
         ${scaledFontSize(3)};
-        font-family: 'Noto Serif', serif;
+        font-family: 'Simonetta-Black', serif;
         margin-bottom: 0;
       }
       h4 {
         ${scaledFontSize(2)};
-        font-family: 'Noto Serif', serif;
+        font-family: 'Simonetta-Black', serif;
         margin-bottom: 0;
       }
       h5 {
         ${scaledFontSize(1)};
-        font-family: 'Noto Serif', serif;
+        font-family: 'Simonetta-Black', serif;
         margin-bottom: 0;
       }
     `}
