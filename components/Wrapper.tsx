4d7e6133732df84ef719899a6abf5160cf695c8b
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2022-11-20 19:26:44 +0100

    🎨 50rem max width for content.

--- components/Wrapper.tsx
+++ components/Wrapper.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from 'react'
 
 export const Wrapper: React.FC<PropsWithChildren<unknown>> = ({ children }) => {
-  return <div className="max-w-3xl px-4 mx-auto">{children}</div>
+  return <div className="max-w-[50rem] px-4 mx-auto">{children}</div>
 }
