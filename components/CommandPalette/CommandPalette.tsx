2775ecd906cdf37f7d861ed6a00c539b579ed156
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2023-01-05 18:09:30 +0100

    🐛max width for cmd+k again.

--- components/CommandPalette/CommandPalette.tsx
+++ components/CommandPalette/CommandPalette.tsx
@@ -7,7 +7,7 @@ export const CommandPalette = () => {
       {/* Renders the content outside the root node */}
       <KBarPositioner className="backdrop-blur-sm">
         {/* Centers the content */}
-        <KBarAnimator className="max-w-2xl w-full bg-surface dark:bg-surface-dark rounded-lg overflow-hidden border-border dark:border-border-dark shadow-lg">
+        <KBarAnimator className="max-w-p w-full bg-surface dark:bg-surface-dark rounded-lg overflow-hidden border-border dark:border-border-dark shadow-lg">
           {/* Handles the show/hide and height animations */}
           <KBarSearch className="px-4 py-1 w-full outline-none border-0 border-b-2 " />
           <Results />
