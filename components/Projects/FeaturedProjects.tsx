8378fb2ca3b5c2b24e658f3495dc9ddda1680649
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2023-01-18 18:02:35 +0100

    🎨 feature projects without images.

--- components/Projects/FeaturedProjects.tsx
+++ components/Projects/FeaturedProjects.tsx
@@ -7,9 +7,13 @@ export const FeaturedProjects = () => {
   return (
     <section>
       <SmallTitle>Projects</SmallTitle>
-      <div className="grid md:grid-cols-2 justify-items-start gap-4">
-        {projectsDB.slice(0, 2).map((project) => (
-          <ProjectCard project={project} key={project.title} />
+      <div className="grid md:grid-cols-3 justify-items-start gap-4">
+        {projectsDB.slice(0, 3).map((project) => (
+          <ProjectCard
+            showImage={false}
+            project={project}
+            key={project.title}
+          />
         ))}
       </div>
       <div className="grid place-items-center md:place-items-start">
--- components/Projects/ProjectCard.tsx
+++ components/Projects/ProjectCard.tsx
@@ -3,14 +3,20 @@ import { ProjectType } from './projectsDB'
 
 type ProjectCardProps = {
   project: ProjectType
+  showImage?: boolean
 }
 
-export const ProjectCard = ({ project }: ProjectCardProps) => {
+export const ProjectCard = ({
+  project,
+  showImage = true,
+}: ProjectCardProps) => {
   return (
     <Card className="max-w-[20rem] flex flex-col">
       <h3 className="text-4xl my-0 ">{project.title}</h3>
       <h4 className="text-lg mt-0">{project.subtitle}</h4>
-      <img src={project.image} alt={project.title} className="mx-auto my-6" />
+      {showImage && (
+        <img src={project.image} alt={project.title} className="mx-auto my-6" />
+      )}
       <p className="flex-grow">{project.description}</p>
       <div className="grid place-items-center md:place-items-start">
         {project.links.map((link) => (
