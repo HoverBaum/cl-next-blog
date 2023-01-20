2c2b1e76a4ab170325d44f1e3efcdba9b715cdab
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2023-01-20 12:15:25 +0100

    ✨ update app header.

--- app/head.tsx
+++ app/head.tsx
@@ -2,6 +2,16 @@ export default function Head() {
   return (
     <>
       <title>Hendriks Blog</title>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1"
+      ></meta>
+      <meta name="monetization" content="$ilp.uphold.com/DbnmiKGQ4Khj" />
+      <script
+        async
+        defer
+        src="https://app.neoanalytics.de/neoClient.js"
+      ></script>
     </>
   )
 }
