c8676aa1a60d71f815f880945b4fdd2fa3f2749e
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2023-01-20 15:30:09 +0100

    🔥 remove console.log.

--- app/posts/tags/[tagSlug]/head.tsx
+++ app/posts/tags/[tagSlug]/head.tsx
@@ -8,8 +8,8 @@ export default function Head({
   params: { tagSlug: string }
 }) {
   const tag = tagsFromPosts(allPosts).find((tag) => tag.tagSlug === tagSlug)
-  console.log('TAG', tag)
   const title = `Hendriks Blog | ${tag?.name}`
+
   return (
     <>
       <SharedHead />
