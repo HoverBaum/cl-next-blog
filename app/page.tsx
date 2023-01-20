50fe655d0f61395afe98b5426bd9a2020b68e2b2
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2023-01-20 13:09:21 +0100

    ▲ migrate home to app folder.

---
+++ app/page.tsx
@@ -0,0 +1,50 @@
+import { compareDesc } from 'date-fns'
+import { MeIntro } from 'components/MeIntro'
+import { PostCard } from 'components/Posts/PostCard'
+import { SmallTitle } from 'components/SmallTitle'
+import { Wrapper } from 'components/Wrapper'
+import { allPosts } from 'contentlayer/generated'
+import { LinkButton } from 'components/LinkButton'
+import { RecentTalks } from 'components/Talks/RecentTalks'
+import { FeaturedProjects } from 'components/Projects/FeaturedProjects'
+import { NextSteps } from 'components/NextSteps/NextSteps'
+import { NextAboutMe } from 'components/NextSteps/NextAboutMe'
+import { NextPosts } from 'components/NextSteps/NextPosts'
+import { NextTalks } from 'components/NextSteps/NextTalks'
+
+const posts = allPosts
+  .sort((a, b) => {
+    return compareDesc(new Date(a.date), new Date(b.date))
+  })
+  .slice(0, 3)
+
+export default async function Home() {
+  return (
+    <Wrapper>
+      <MeIntro />
+
+      <SmallTitle>Current post</SmallTitle>
+      <PostCard post={posts[0]} />
+
+      <SmallTitle>Recent posts</SmallTitle>
+      <div className="mb-6 grid md:grid-cols-2 gap-6">
+        <PostCard post={posts[1]} />
+        <PostCard post={posts[2]} />
+      </div>
+      <div className="grid place-items-center md:place-items-start">
+        <LinkButton href="/posts">All Posts</LinkButton>
+      </div>
+
+      <SmallTitle>Latest Talks</SmallTitle>
+      <RecentTalks />
+
+      <FeaturedProjects />
+
+      <NextSteps>
+        <NextAboutMe />
+        <NextPosts />
+        <NextTalks />
+      </NextSteps>
+    </Wrapper>
+  )
+}
--- components/LinkButton.tsx
+++ components/LinkButton.tsx
@@ -1,5 +1,4 @@
-import { useRouter } from 'next/router'
-import { Button } from './Button'
+import Link from 'next/link'
 
 type LinkButtonProps = {
   children: React.ReactNode
@@ -8,15 +7,12 @@ type LinkButtonProps = {
 }
 
 export const LinkButton = ({ children, href, className }: LinkButtonProps) => {
-  const router = useRouter()
   return (
-    <Button
-      className={className}
-      onClick={() => {
-        router.push(href)
-      }}
+    <Link
+      className={`hover-shadow m-2 inline-block cursor-pointer font-semibold rounded-sm outline-none px-4 py-2 border-2 border-primary dark:border-primary-dark bg-background dark:bg-background-dark no-underline hover:no-underline hover:text-text hover:dark:text-text-dark ${className}`}
+      href={href}
     >
       {children}
-    </Button>
+    </Link>
   )
 }
