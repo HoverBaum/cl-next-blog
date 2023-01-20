581c19a1fbbd40d51cd93b74cfb12b39170e0508
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2023-01-20 09:28:58 +0100

    ✨ resonable meIntro with Image.

--- components/MeIntro.tsx
+++ components/MeIntro.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import { WigglingEmoji } from './WigglingEmoji'
 
@@ -6,7 +7,7 @@ export const MeIntro = () => {
   return (
     <div className="p-4 border-2 border-border dark:border-border-dark ">
       <div className="md:flex justify-between">
-        <div className="w-full">
+        <div className="">
           <p className="text-xl md:text-2xl mt-0">
             » Hi, I am Hendrik <WigglingEmoji>👋</WigglingEmoji> <br />
             JavaScript Enthusiast and developer for fun!
@@ -15,11 +16,16 @@ export const MeIntro = () => {
           </p>
           <Link href="/me">More about me</Link>
         </div>
-        <img
-          className="mt-4 md:mt-0 w-full md:w-auto md:h-[10rem] border border-border dark:border-border-dark shadow-[4px_6px_0_0] shadow-primary dark:shadow-primary-dark"
-          src="/images/assets/portrait.jpg"
-          alt="Hendrik"
-        />
+        <div className="flex md:block flex-row justify-center mt-4 md:mt-0">
+          <span className="text-[0] inline-block border border-border dark:border-border-dark shadow-[4px_6px_0_0] shadow-primary dark:shadow-primary-dark">
+            <Image
+              src="/images/assets/portrait.jpg"
+              alt="Hendrik"
+              width={250}
+              height={250}
+            />
+          </span>
+        </div>
       </div>
     </div>
   )
