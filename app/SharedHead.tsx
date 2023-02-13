6fef60dd975249ce0a48d9b26803a3cb60b277ee
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2023-02-13 13:20:29 +0100

    👩‍🎤 add neo client.

--- app/SharedHead.tsx
+++ app/SharedHead.tsx
@@ -6,6 +6,11 @@ export const SharedHead = () => {
         content="width=device-width, initial-scale=1"
       ></meta>
       <meta name="monetization" content="$ilp.uphold.com/DbnmiKGQ4Khj" />
+      <script
+        async
+        defer
+        src="https://app.neoanalytics.de/neoClient.js"
+      ></script>
     </>
   )
 }
